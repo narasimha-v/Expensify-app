eca4046039f09146559d35c087cb0d38093df248
Why-just-why <54150780+Why-just-why@users.noreply.github.com>
2020-03-29 18:43:55 +0530

    changes to App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import AppRouter from "./routers/AppRouter";
 import { Provider } from "react-redux";
 import configureStore from "./store/configureStore";
-import { addExpense } from "./actions/expenses";
 
 const store = configureStore();
 
