71e4836d4cfa8b076153e8c74befd4791fc9d91d
Why-just-why <54150780+Why-just-why@users.noreply.github.com>
2020-03-31 00:27:14 +0530

    display items fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import AppRouter from "./routers/AppRouter";
 import { Provider } from "react-redux";
 import configureStore from "./store/configureStore";
-
+import { SetExpenses } from "./actions/expenses";
 const store = configureStore();
-
 const App = () => {
+  let [display, setDisplay] = useState(false);
+  store.dispatch(SetExpenses()).then(() => setDisplay((display = true)));
   return (
     <div>
-      <Provider store={store}>
-        <AppRouter />
-      </Provider>
+      {display ? (
+        <Provider store={store}>
+          <AppRouter />
+        </Provider>
+      ) : (
+        <p>Loading....</p>
+      )}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,15 +3,8 @@ import ReactDOM from "react-dom";
 import "./styles/styles.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { SetExpenses } from "./actions/expenses";
-import configureStore from "./store/configureStore";
 
-const store = configureStore();
-
-ReactDOM.render(<p>Loading....</p>, document.getElementById("root"));
-store.dispatch(SetExpenses()).then(() => {
-  ReactDOM.render(<App />, document.getElementById("root"));
-});
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
